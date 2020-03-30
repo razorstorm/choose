9f5e370b2934d60a2b561fe6e25a65a454248f62
Jason Jiang <jjiang1991@gmail.com>
2020-03-30 15:24:41 -0700

    Do not random the same thing twice

--- background.js
+++ background.js
@@ -2,31 +2,7 @@ console.log('bg starting')
 chrome.runtime.onInstalled.addListener(function () {
     console.log('starting')
     chrome.storage.sync.set({ chooseColor: '#3aa757' }, function () {
-        console.log("The color is set.");
     });
-
-    // A generic onclick callback function.
-    function genericOnClick(info, tab) {
-        console.log("item " + info.menuItemId + " was clicked");
-        console.log("info: " + JSON.stringify(info));
-        console.log("tab: " + JSON.stringify(tab));
-    }
-
-    console.log(chrome, chrome.contextMenus)
-
-    // Create one test item for each context type.
-    var contexts = ["all"];
-    for (var i = 0; i < contexts.length; i++) {
-        var context = contexts[i];
-        var title = "Test '" + context + "' menu item";
-        var id = chrome.contextMenus.create({
-            id: "all",
-            "title": title, "contexts": [context],
-            // "onclick": genericOnClick
-        });
-        console.log("'" + context + "' item:" + id);
-    }
-    chrome.contextMenus.onClicked.addListener(genericOnClick)
 });
 
 chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
--- popup.js
+++ popup.js
@@ -1,33 +1,40 @@
-// TODO:
-// crawl through children tree, and sort through _all_ bookmarks. Just do a flatMap
-// context menu
-
-// let changeColor = document.getElementById('changeColor');
-
-// changeColor.onclick = async function (element) {
-//     // li.append(dumpTreeNodes(bookmarkNode.children, query));
-//     // dumpBookmarks("Karen");
-// };
+const MAX_TRIES = 10;
 
 // Traverse the bookmark tree, and print the folder and nodes.
-function openRandom(query) {
-    chrome.bookmarks.getTree(
-        function (bookmarkTreeNodes) {
-            const queriedChildren = grabFolderNodes(query, bookmarkTreeNodes[0].children[0].children)[0].children
-            queriedChildren.forEach(child => console.log(child));
+async function openRandom(query) {
+    const bookmarkTreeNodes = await chrome.bookmarks.getTree();
+    const queriedNode = grabFolderNodes(query, bookmarkTreeNodes[0].children[0].children)[0]
+    const queriedChildren = crawlTree(queriedNode);
+    const key = `lastChild:${query}`;
 
-            const randomChild = queriedChildren[Math.floor(Math.random() * queriedChildren.length)];
-            console.log('RANDOM WAS', randomChild.title);
-            chrome.tabs.create({
-                url: randomChild.url
-            })
+    let last = await chrome.storage.sync.get(key)
+    console.debug("last randomed:", last, last[key]);
+
+    let randomChild;
+    let found = false;
+    let tries = 0;
+    do {
+        randomChild = queriedChildren[Math.floor(Math.random() * queriedChildren.length)];
+        console.debug('RANDOM WAS', randomChild.url, 'last was:', last, last[key]);
+        if (randomChild && (!last || last[key] !== randomChild.url)) {
+            found = true
         }
-    );
+        tries++;
+    } while (!found && tries < MAX_TRIES);
+
+    const update = {}
+    update[key] = randomChild.url;
+    await chrome.storage.sync.set(update);
+
+    console.debug("FOUND:" + randomChild.title)
+    console.debug("----------------------------------");
+    chrome.tabs.create({
+        url: randomChild.url
+    });
 }
 
 async function grabAllBookmarkFolders() {
     var bookmarkTreeNodes = await chrome.bookmarks.getTree()
-
     const folders = bookmarkTreeNodes[0].children[0].children;
     return folders.filter(node => node.children)
 }
@@ -38,31 +45,35 @@ function grabFolderNodes(query, bookmarkNodes) {
         return node.children && hasQuery;
     })
 }
-function dumpTreeNodes(bookmarkNodes, query) {
-    var list = $('<ul>');
-    var i;
-    for (i = 0; i < bookmarkNodes.length; i++) {
-        list.append(dumpNode(bookmarkNodes[i], query));
+
+function crawlTree(root) {
+    let results = [];
+    let toSee = [root];
+    while (toSee && toSee.length > 0) {
+        let element = toSee.pop();
+        if (element) {
+            if (!element.children || element.children.length === 0) {
+                results.push(element);
+            } else {
+                toSee = [...toSee, ...element.children];
+            }
+        }
     }
-    return list;
+    return results
 }
 
 document.addEventListener('DOMContentLoaded', async function () {
-    // dumpBookmarks("Karen");
     const results = await grabAllBookmarkFolders();
-    console.log(results);
     results.forEach(result => {
         $('#buttons').append(`<button class="bookmark" style="width: 200px" key="${result.title}">${result.title}</button>`);
     });
 
     const bookmarkButtons = document.getElementsByClassName('bookmark');
-    console.log(bookmarkButtons)
 
     for (let item of bookmarkButtons) {
-        console.log(item);
         item.onclick = async function (element) {
-            console.log(element.target, element.target.attributes.key.value);
-            openRandom(element.target.attributes.key.value);
+            console.debug("Clicked on folder", element.target, element.target.attributes.key.value);
+            await openRandom(element.target.attributes.key.value);
         }
     }
 });
